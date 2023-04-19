5a42f36d5c85171c0ca7e602f00d999dc5f14ac5
alenjohn05 <alenjohnmj@gmail.com>
2023-04-19 15:32:00 +0530

    This is a test commit

---
+++ fylo/src/presentation/components/PageTitle/PageTitleWithOutButton.tsx
@@ -0,0 +1,41 @@
+import { FC } from 'react';
+import PropTypes from 'prop-types';
+import AddTwoToneIcon from '@mui/icons-material/AddTwoTone';
+import { Typography, Button, Grid } from '@mui/material';
+
+interface PageTitleWithOutButtonProps {
+  heading?: string;
+  subHeading?: string;
+  docs?: string;
+}
+
+const PageTitleWithOutButton: FC<PageTitleWithOutButtonProps> = ({
+  heading = '',
+  subHeading = '',
+  docs = '',
+  ...rest
+}) => {
+  return (
+    <Grid
+      container
+      justifyContent="space-between"
+      alignItems="center"
+      {...rest}
+    >
+      <Grid item>
+        <Typography variant="h3" component="h3" gutterBottom>
+          {heading}
+        </Typography>
+        <Typography variant="subtitle2">{subHeading}</Typography>
+      </Grid>
+    </Grid>
+  );
+};
+
+PageTitleWithOutButton.propTypes = {
+  heading: PropTypes.string,
+  subHeading: PropTypes.string,
+  docs: PropTypes.string
+};
+
+export default PageTitleWithOutButton;
